469767793e96fd9e5ee1c9fcbc1388a3d681957c
gm1001 <mathuyrgopal@gmail.com>
2023-01-13 19:23:06 +0530

    work done

---
+++ fromfigma.tsx
@@ -0,0 +1,104 @@
+import type { NextPage } from "next";
+
+const Slide1691: NextPage = () => {
+  return (
+    <div className="relative bg-white w-full h-[5239px] overflow-hidden text-left text-xl text-white font-poppins">
+      <img
+        className="absolute top-[1414px] left-[0px] w-[1923px] h-[1123px] object-cover"
+        alt=""
+        src="../dsc-0911-1@2x.png"
+      />
+      <div className="absolute top-[0px] left-[0px] [background:linear-gradient(90.55deg,_#180613_35.86%,_#d9d9d9)] w-[1920px] h-[1091px]" />
+      <img
+        className="absolute top-[0px] left-[817px] w-[1103px] h-[1103px] object-cover"
+        alt=""
+        src="../ritwij-celestial-and-space-theme-background-for-website-with-sa-b2e63cc0d7f94558ba994d51d1999e0b-1@2x.png"
+      />
+      <nav className="absolute top-[41px] left-[396px] w-[1128.46px] h-[50px]">
+        <div className="absolute top-[0px] left-[0px] text-base font-poppins text-white text-left">
+          HOME
+        </div>
+        <div className="absolute top-[0px] left-[152.03px] text-base font-poppins text-white text-left">
+          ABOUT US
+        </div>
+        <div className="absolute top-[0px] left-[369.76px] text-base font-poppins text-white text-left">
+          ITINERARY
+        </div>
+        <div className="absolute top-[0px] left-[583.74px] text-base font-poppins text-white text-left">
+          EVENTS
+        </div>
+        <div className="absolute top-[0px] left-[758.3px] text-base font-poppins text-white text-left">
+          SPONSOR
+        </div>
+        <div className="absolute top-[0px] left-[969.46px] text-base font-poppins text-white text-left">
+          CONTACT
+        </div>
+      </nav>
+      <img
+        className="absolute top-[29px] left-[57px] w-[168px] h-[73px] object-cover"
+        alt=""
+        src="../ecell-logo-white-1@2x.png"
+      />
+      <h1 className="m-0 absolute top-[322px] left-[64px] text-[136.26px] font-bold font-inherit">
+        E-SUMMIT 23’
+      </h1>
+      <p className="m-0 absolute top-[526px] left-[64px] text-[24px] inline-block w-[815px] h-[181px]">
+        Entrepreneurship Cell organises North India’s Biggest Entrepreneurship
+        Summit 2023. It is a two day event wherein we start with
+        Entrepreneurship Events in the morning and Cultural nights in the
+        evening.
+      </p>
+      <h4 className="m-0 absolute top-[669px] left-[64px] text-[42.67px] font-bold font-inherit">
+        4th-5th February 2023
+      </h4>
+      <div className="absolute top-[760px] left-[64px] w-[407px] h-20 text-[34.62px] text-gray-100">
+        <div className="absolute top-[0px] left-[0px] rounded-[69px] bg-white w-[407px] h-20" />
+        <b className="absolute top-[14px] left-[74px]">REGISTER NOW</b>
+      </div>
+      <div className="absolute top-[1086px] left-[-3px] bg-purple w-[1923px] h-[328px]" />
+      <div className="absolute top-[1133px] left-[76px] w-[492px] h-[223px]">
+        <b className="absolute top-[0px] left-[0px]">40,000+</b>
+        <div className="absolute top-[163px] left-[133px] text-lg">
+          FOOTFALL
+        </div>
+      </div>
+      <div className="absolute top-[1133px] left-[844px] w-[271px] h-[223px] text-lg">
+        <input
+          className="cursor-pointer absolute top-[0px] left-[80px] text-xl font-bold font-poppins text-white text-left"
+          type="checkbox"
+        >
+          12
+        </input>
+        <div className="absolute top-[163px] left-[0px]">Competitions</div>
+      </div>
+      <div className="absolute top-[1133px] left-[1433px] w-[390px] h-[223px]">
+        <b className="absolute top-[0px] left-[84px]">30+</b>
+        <div className="absolute top-[163px] left-[0px] text-lg">{`SPEAKERS & ARTISTS`}</div>
+      </div>
+      <div className="absolute top-[1414px] left-[0px] bg-gray-200 w-[1920px] h-[1125px]" />
+      <b className="absolute top-[1527px] left-[76px] text-[106.34px]">{`About Us `}</b>
+      <div className="absolute top-[1749px] left-[76px] text-[32px] font-light inline-block w-[897px] h-[619px]">
+        <p className="[margin-block-start:0] [margin-block-end:0px]">
+          The Annual Flagship Extravaganza hosted by Entrepreneurship Cell of
+          Delhi Technological University. E-Summit by E-Cell
+        </p>
+        <p className="m-0">
+          DU is the largest entrepreneurship promoting event of Northern India.
+          The event has served as host to some of the eminent personalities from
+          myriad industries over the many years. It continues to cultivate and
+          nurture numerous ideas every year, and provide an extraordinary
+          platform putting forward the youth to exchange their ideas with the
+          sharpest brains of the corporate world, giving them innumerous
+          insights and lessons to raise their business acumen.
+        </p>
+      </div>
+      <img
+        className="absolute top-[1510px] left-[1150px] w-[600px] h-[899.89px] object-cover"
+        alt=""
+        src="../dsc-0067-1@2x.png"
+      />
+    </div>
+  );
+};
+
+export default Slide1691;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Navbar from "../components/Navbar";
+import Image from "next/image";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className = "bg-background font-poppins">
+      {/* <Navbar/> */}
+      <div className=" top-[0px] right-0 absolute shadow-none">
+        <Image height={600} width={600} src="/celestial_and_space.png" alt="" />
+      </div>
+      <Component {...pageProps} />
+    </div>
+  );
 }
