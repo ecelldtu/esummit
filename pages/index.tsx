390e346c36222053f7f24a19d17f959450c3534b
GM-11 <gmathur1001@gmail.com>
2023-01-16 17:35:33 +0530

    removed redundant code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,7 @@
-import Navbar from "../components/Navbar";
 import Home from "./screens/home";
-import Image from "next/image";
 export default function Root() {
   return (
     <div className="bg-background font-poppins w-[100%] m-0 ">
-      
-      <Home />
-
       <Home />
     </div>
   );
