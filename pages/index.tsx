d6d6b5730a8d7972fd0543af310033e7defe1c89
aaditya9899 <aadityaprasad9899@gmail.com>
2023-01-16 14:30:21 +0530

    footer done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 
 import Home from './screens/home'
+import About from './screens/about'
 
 
 export default function Root() {
   return (
     <>
     <Home/>
+    <About/>
     {/* <Home/> */}
    
     </>
