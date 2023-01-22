78dee85068f65c946a8f95854abd27d942ff674e
GargDivanshu <divanshu.garg113@gmail.com>
2023-01-22 11:59:55 +0530

    tsconfig + nect.config change

--- next.config.js
+++ next.config.js
@@ -3,14 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
-// module.exports = {
-//   typescript: {
-//     // !! WARN !!
-//     // Dangerously allow production builds to successfully complete even if
-//     // your project has type errors.
-//     // !! WARN !!
-//     ignoreBuildErrors: true,
-//   },
-//   reactStrictMode: true,
-// }
+// module.exports = nextConfig
+module.exports = {
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  reactStrictMode: true,
+}
