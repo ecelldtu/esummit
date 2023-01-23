5811e636c4e1635eea5202f07dca95e5663c97de
GargDivanshu <divanshu.garg113@gmail.com>
2023-01-23 20:25:18 +0530

    sponosrs

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,6 +6,7 @@ import Home from "./screens/home";
 import Itenaryscreen from "./screens/itenaryscreen";
 import Sponsors from './../components/Sponsors';
 import Motivation from './../components/Motivation';
+import PastSpeakers from "../components/PastSpeakers"
 
 export default function Root() {
 
