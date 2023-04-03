2589f357359620b3474691dc5e50037559b90fc4
aaditya9899 <aadityaprasad9899@gmail.com>
2023-04-03 12:15:54 +0530

    home button fixed in navbar

--- pages/screens/sponsors.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Sponsors from '../../components/Sponsors'
-
-export default function sponsors() {
-  return (
-    <Element name="sponsors">
-    <Sponsors></Sponsors></Element>
-  )
-}
