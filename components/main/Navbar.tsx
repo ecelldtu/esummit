06cf250c5915c52bd0cba713d45d63b1e5c26721
RajKunx2 <113442064+RajKunx2@users.noreply.github.com>
2023-12-23 21:52:55 +0530

    fixed

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -11,16 +11,16 @@ const Navbar = () => {
 
   return (
     <div className="w-screen h-auto py-4 fixed top-0 shadow-lg font-CyberSport shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md px-10 z-[1000]">
-      <div className={`w-full bg-violet-500 top-0 left-0 h-8 fixed z-[10000] font-Para font-extrabold flex flex-row justify-between ${isBannerVisible ? '' : 'hidden'}`}>
-        <div className="my-auto ml-2 md:ml-4 text-sm font-medium md:text-xl">
+      <div className={`w-full bg-violet-500 top-0 left-0 h-12 fixed z-[10000] font-Para font-extrabold flex flex-row justify-between ${isBannerVisible ? '' : 'hidden'}`}>
+        <div className="my-auto ml-2 md:ml-4 text-sm font-extrabold md:text-xl text-white">
           E-Cell DTU Official Website Launched
-          <a href="https://ecelldtu.in/" className="my-auto mx-4 underline-offset-4 underline text-sm md:text-sm text-white">KNOW MORE</a>
         </div>
-        <button className="my-auto mr-2 md:mr-4 text-white" onClick={hideBanner}>
+        <a href="https://ecelldtu.in/" className="font-extrabold my-auto hover:bg-white/80 w-40 md:w-auto scale-75 mr-[20px] md:scale-100 md:-mr-[560px] xl:ml-[360px] bg-white text-violet-500 text-sm md:text-base px-4 rounded-2xl py-2 md:py-1">KNOW MORE</a>
+        <button className="my-auto mr-2 md:mr-12 text-white" onClick={hideBanner}>
           X
         </button>
       </div>
-      <div className={`w-full ${isBannerVisible ? 'mt-7' : ''} h-full flex flex-row items-center justify-center md:justify-between m-auto px-[10px]`}>
+      <div className={`w-full ${isBannerVisible ? 'mt-12' : ''} h-full flex flex-row items-center justify-center md:justify-between m-auto px-[10px]`}>
         <a href="#about-me" className="h-auto w-auto flex flex-row items-center">
           <Image
             src="/logo.png"
