14aee841b4e6d7fefcd0eec88d12cc2752d0a44d
RajKunx2 <113442064+RajKunx2@users.noreply.github.com>
2023-12-28 01:01:40 +0530

    ohk done

--- components/main/Events.jsx
+++ components/main/Events.jsx
@@ -19,7 +19,7 @@ const GalleryComponent = () => {
         <>
             <h1 className='text-center text-5xl md:text-8xl mt-16 md:mt-32 md:mb-12 xl:text-9xl w-full text-white py-4 font-CyberGraph'>EVENTS</h1>
             <div className="flex h-auto items-center z-[500] justify-center">
-                <div className="grid grid-cols-1 gap-24 md:grid-cols-2 lg:grid-cols-3">
+                <div className="grid grid-cols-1 gap-12 md:grid-cols-2 lg:grid-cols-3">
                     {galleryData.map((item, index) => (
                         <motion.div ref={ref}
                             animate={controls}
@@ -29,8 +29,8 @@ const GalleryComponent = () => {
                                 visible: { opacity: 1, y: 0 },
                                 hidden: { opacity: 0, y: 60 },
                             }} key={index} className="group relative cursor-pointer rounded-xl items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
-                            <div className="h-96 w-72">
-                                <Image width={400} height={400} className="h-full rounded-xl w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125" src={item.imageUrl} alt={item.title} />
+                            <div className="h-72 w-72 mx-auto">
+                                <Image width={400} height={400} className="h-full rounded-xl w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125" src={item.imageUrl} alt={""} />
                             </div>
                             <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
                             <div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
@@ -51,10 +51,24 @@ const GalleryComponent = () => {
 const galleryData = [
     {
         title: 'ECELL CONCLAVE',
-        imageUrl: 'https://images.unsplash.com/photo-1524504388940-b1c1722653e1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+        imageUrl: 'https://images.unsplash.com/photo-1568992687947-868a62a9f521?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         href: "/Conclave",
         time: "10 AM - 4 PM",
         location: "DTU MAIN CAMPUS"
+    },
+    {
+        title: 'COMING SOON',
+        imageUrl: '',
+        href: "/",
+        time: "COMING SOON",
+        location: "COMING SOON"
+    },
+    {
+        title: 'COMING SOON',
+        imageUrl: '',
+        href: "/",
+        time: "COMING SOON",
+        location: "COMING SOON"
     },
 ];
 
