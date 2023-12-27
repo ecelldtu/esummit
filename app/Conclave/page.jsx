93817f739823a8a07efbe395b190991e38d22c26
RajKunx2 <113442064+RajKunx2@users.noreply.github.com>
2023-12-27 18:26:13 +0530

    sad

--- app/Conclave/page.jsx
+++ app/Conclave/page.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import Image from "next/image"
 
-export default function pages() {
-  return (
-    <div>pages</div>
-  )
+export default function page() {
+    return (
+        <>
+            <div className="w-screen h-auto py-4 fixed top-0 shadow-lg font-CyberSport shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md px-10 z-[1000]">
+                <div className="w-full h-full flex flex-row items-center justify-center md:justify-between m-auto px-[10px]">
+                    <a href="/" className="h-auto w-full flex flex-row items-center">
+                        <Image
+                            src="/logo.png"
+                            alt="logo"
+                            width={120}
+                            height={80}
+                            className="cursor-pointer hover:animate-slowspin mx-auto"
+                        />
+                    </a>
+                </div>
+            </div>
+            <h1 className='text-center mt-24 text-5xl md:text-8xl xl:text-9xl w-full text-white py-4 font-Para'>ITINERARIES</h1>
+            <p>ECell DTU extends a warm and exclusive invitation,inviting you to immerse yourself in the world ofentrepreneurship ON 28TH OF JANUARY 2024.
+            </p>
+            <p>Join us for an exploration of innovation,collaboration, and empowerment, where aspiringentrepreneurs from 30+ E Cell’s converge to shareideas, gain insights, and chart the course forfuture success. Your presence at E-Cell Conclaveis not just an invitation; it&lsquo;s an opportunity to bepart of a thriving community that celebrates thespirit of innovation and fosters the entrepreneurialmindset. We look forward to hosting you at thisenriching event, where possibilities unfold andconnections flourish.</p>
+        </>
+    )
 }
--- components/main/Events.jsx
+++ components/main/Events.jsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useRef, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useInView } from 'react-intersection-observer';
 import { motion, useAnimation } from 'framer-motion';
 import Image from 'next/image';
@@ -28,15 +28,16 @@ const GalleryComponent = () => {
                             variants={{
                                 visible: { opacity: 1, y: 0 },
                                 hidden: { opacity: 0, y: 60 },
-                            }} key={index} className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+                            }} key={index} className="group relative cursor-pointer rounded-xl items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
                             <div className="h-96 w-72">
-                                <Image width={400} height={400} className="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125" src={item.imageUrl} alt={item.title} />
+                                <Image width={400} height={400} className="h-full rounded-xl w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125" src={item.imageUrl} alt={item.title} />
                             </div>
                             <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
                             <div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
-                                <h1 className="font-dmserif text-3xl font-bold text-white">{item.title}</h1>
-                                <p className="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">{item.description}</p>
-                                <button className="rounded-full py-2 px-3.5 font-com text-base capitalize bg-violet-600 text-white shadow shadow-black/60">Know More</button>
+                                <h1 className="font-CyberSport my-2 text-4xl font-bold text-white">{item.title}</h1>
+                                <p className="mb-3 text-lg font-Queensides my-2 text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">{item.time}</p>
+                                <p className="mb-3 text-lg font-Para my-2 font-bold text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">{item.location}</p>
+                                <a href={item.href} className="font-CyberGraph rounded-full my-2 py-2 px-3.5 font-com text-base capitalize bg-violet-600 text-white shadow shadow-black/60">Know More</a>
                             </div>
                         </motion.div>
                     ))}
@@ -49,19 +50,11 @@ const GalleryComponent = () => {
 
 const galleryData = [
     {
-        title: 'Beauty',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis dolore adipisci placeat.',
+        title: 'ECELL CONCLAVE',
         imageUrl: 'https://images.unsplash.com/photo-1524504388940-b1c1722653e1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
-    },
-    {
-        title: 'Beyond Builder',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis dolore adipisci placeat.',
-        imageUrl: 'https://images.unsplash.com/photo-1494145904049-0dca59b4bbad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80',
-    },
-    {
-        title: 'Shooting Star',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis dolore adipisci placeat.',
-        imageUrl: 'https://images.unsplash.com/photo-1502675135487-e971002a6adb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80',
+        href: "/Conclave",
+        time: "10 AM - 4 PM",
+        location: "DTU MAIN CAMPUS"
     },
 ];
 
