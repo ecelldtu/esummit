188063ae45812ea322c23cd872c0863aeb4c20a7
RajKunx2 <113442064+RajKunx2@users.noreply.github.com>
2025-01-09 00:33:13 +0530

    Update page.jsx

--- src/app/About/page.jsx
+++ src/app/About/page.jsx
@@ -11,8 +11,7 @@ export default function BoxRevealDemo() {
 
             <BoxReveal boxColor={"#5046e6"} duration={0.5}>
                 <h2 className="mt-[.5rem] text-[1rem] font-Darker text-[#fff] max-w-[80%] mx-auto text-xl md:text-3xl md:max-w-[60%] text-center font-semibold">
-                    E-Cell DTU presents its annual flagship event E-SUMMIT'24 One of the biggest Entrepreneurship summits in India, E-Summit will bring to you a series of enthralling events. from panel discussions with distinguished dignitaries.to exciting competitions like B-plan.
-                    Case study, hackathon, along with a spectacular band night and comedy night, the level of grandeur will be unparalleled
+                    E-Cell DTU presents its annual flagship event E-SUMMIT'25 One of the biggest Entrepreneurship summits in India, E-Summit will bring to you a series of enthralling events. from panel discussions with distinguished dignitaries.to exciting competitions like B-plan, Case study and Hackathon, along with a spectacular band night and comedy night, the level of grandeur will be unparalleled
                 </h2>
             </BoxReveal>
         </div>
